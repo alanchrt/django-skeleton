125e3124e4bdf17c8f214b04e52d2ed56b0710e0
Alan Christopher Thomas <alan.thomas@liquid9.tv>
2011-01-17 14:40:52 -0600

    fixed project path

--- manage.py
+++ manage.py
@@ -6,8 +6,7 @@ import shutil
 import string
 from django.core.management import execute_manager
 
-project = lambda path: os.path.join(os.path.dirname(
-                                            os.path.dirname(__file__)), path)
+project = lambda path: os.path.join(os.path.dirname(__file__), path)
 
 if __name__ == "__main__":
     if sys.argv[1] == 'configure':
