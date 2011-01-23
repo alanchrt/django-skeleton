c2713f5626dd6fe65b449fe63d7e970b892d5cfa
Alan Christopher Thomas <alan.christopher.thomas@gmail.com>
2011-01-23 04:31:34 -0600

    removed trailing whitespace

--- settings/environment.example.py
+++ settings/environment.example.py
@@ -10,16 +10,16 @@ MANAGERS = ADMINS
 SECRET_KEY = ''
 
 MEDIA_URL = '/media/'
-ADMIN_MEDIA_PREFIX = 'http://localhost:8000/media/admin/' 
+ADMIN_MEDIA_PREFIX = 'http://localhost:8000/media/admin/'
 CACHE_BACKEND = 'dummy://'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'database.sqlite',                         
-        'USER': '',                         
-        'PASSWORD': '',                 
-        'HOST': '',                                    
+        'NAME': 'database.sqlite',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '',
     }
 }
